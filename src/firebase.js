5676fae505e6089681cc7e53b48f8ed40dc353d0
Eddy Roberto <eddyr.morales@gmail.com>
2021-02-26 16:08:50 -0600

    Remove config

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyD3MgAvvW4gtv3EDEC3ss8-LlfnnnxioUg",
-  authDomain: "facebook-messenger-42eb9.firebaseapp.com",
-  projectId: "facebook-messenger-42eb9",
-  storageBucket: "facebook-messenger-42eb9.appspot.com",
-  messagingSenderId: "795691417495",
-  appId: "1:795691417495:web:a2f1690d47907c670d4ed2"
+  // Put firebase app config here
 })
 
 const db = firebaseApp.firestore()
